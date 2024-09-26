cbce6c75bbf15bdba620bba3796ac55113839a73
karidiiita <malahovakarinv@gmail.com>
2024-09-26 11:37:59 +0300

    Update lab1.js

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -1,25 +1,31 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?"); 
 console.log("Вы посмотрели ", numberOfFilms, " фильмов.");
 
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
+    count : numberOfFilms, 
+    movies : {
+
+    },
 };
 
-do {
-    let lastSeenFilm = prompt("Какой фильм посмотрели последним?");
-    
-    // Проверка корректности названия фильма
-    if (lastSeenFilm === null  lastSeenFilm.trim() === ""  lastSeenFilm.length > 50) {
-        alert("Неверный ввод! Пожалуйста, введите корректное название фильма (не более 50 символов).");
-        continue; // Вернуться к запросу
+for(let i = 0; i < 2; i++)
+{
+    let lastMovie = prompt('Один из последних просмотренных фильмов?');
+    let movieRating = prompt('На сколько оцените его?');
+
+
+  
+  if(lastMovie != null && movieRating != null 
+    && lastMovie != '' && movieRating != '' 
+    && lastMovie.length < 50 )
+    {
+        personalMovieDB.movies[lastMovie] = movieRating;
     }
+     else
+    {
+        i--
+    }
+}
 
-    let lastFilmRatingt = prompt("На сколько оцените его?");
-    
-    // Здесь можно добавить проверку для рейтинга, если это необходимо
-    personalMovieDB.movies[lastSeenFilm] = lastFilmRatingt;
-    break; // Выйти из цикла после корректного ввода
-} while (true);
 
-console.log(personalMovieDB);
+console.log(personalMovieDB)
