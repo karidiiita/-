d8350df932f628fcdc73169507516899abd71feb
karidiiita <malahovakarinv@gmail.com>
2024-09-26 11:18:10 +0300

    Update lab1.js

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -11,7 +11,15 @@ const personalMovieDB = {
 do{
     let lastSeenFilm = prompt("Какой фильм посмотрели последним?");
     let lastFilmRatingt = prompt("На сколько оцените его?");
-    if(lastSeenFilm == null || lastSeenFilm.length>50 || lastSeenFilm == ""){ 
+    if(lastSeenFilm == null){ 
+        alert("Wrong enter!");
+        continue;
+    }
+     if( lastSeenFilm.length>50"){ 
+        alert("Wrong enter!");
+        continue;
+    }
+     if(lastSeenFilm.length>50){ 
         alert("Wrong enter!");
         continue;
     }
